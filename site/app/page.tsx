27cf9b4e345ee678c75e119f475616400416c942
kjh01072299206-png <kjh01072299206@ajou.ac.kr>
2026-08-01 02:21:20 +0900

    feat: add July 26 top five framing pilot

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,5 +1,14 @@
 import AgendaDashboard from "./agenda-dashboard";
+import Link from "next/link";
 
 export default function Home() {
-  return <AgendaDashboard />;
+  return (
+    <>
+      <Link className="top5-home-banner" href="/top5-2026-07-26">
+        <span><b>7월 26일 상위 5개 의제</b> 본문 기반 구조화 분석을 확인하세요</span>
+        <span>분석 보기 →</span>
+      </Link>
+      <AgendaDashboard />
+    </>
+  );
 }
